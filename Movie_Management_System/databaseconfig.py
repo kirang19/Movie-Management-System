23ba217737f1ef0cebe5112fc8ac3d5e3e4af23e
kiran <kirangiregoudru@gmail.com>
2024-07-30 23:01:48 +0530

    First Commit

---
+++ Movie_Management_System/databaseconfig.py
@@ -0,0 +1,8 @@
+mysql = {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'movie_management_system_',
+    'USER': 'root',
+    'PASSWORD': '12345678',
+    'HOST': 'localhost',
+    'POST': '3306',
+}
